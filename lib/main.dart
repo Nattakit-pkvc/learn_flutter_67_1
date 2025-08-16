8b14237565072399fb928959b9eda0afd4d8b0aa
Nattakit-pkvc <6749010031@phuketvc.ac.th>
2025-08-16 10:31:36 +0700

    Step 5: cpntainer, row, column, main axis, cross axis

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:learn_flutter_67_1/screen/home.dart';
 
 void main() {
   // runApp(const MyApp());
-
   // const app = MaterialApp(title: "หัวข้อ", home: Text("Hello World"));
   // runApp(app);
 
